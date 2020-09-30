6af535f426067725af5e6d4ad5e208c4b77d61aa
fatih cavusoglu <cavusoglu_fatih@hotmail.com>
2020-09-30 11:05:47 +0200

    Aufgabe_12

--- Scripts/Aufgabe_12.sql
+++ Scripts/Aufgabe_12.sql
@@ -1,12 +1,1 @@
-create or replace
-algorithm = UNDEFINED view `vbzdat`.`aufgabe_12` as
-select
-    `a`.`fahrt_id` as `fahrt_id`,
-    date_format(`a`.`datumzeit_soll_ab`, '%H:%i:%s') as `datum`,
-    `l`.`linie` as `linie`
-from
-    (`vbzdat`.`ankunftszeiten` `a`
-join `vbzdat`.`linie` `l` on
-    ((`a`.`fahrweg_id` = `l`.`fahrweg_id`)))
-order by
-    `a`.`delay`
+select;
